390e6155a00eb4d4c1d83b40e7aef5e082b43fe6
Aron Sarosi <aron.sarosi13@gmail.com>
2020-09-01 10:44:33 +0100

    Created streamlit architecture

--- setup.py
+++ setup.py
@@ -1,16 +1,40 @@
 from setuptools import find_packages
 from setuptools import setup
 
-with open('requirements.txt') as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content if 'git+' not in x]
+
+REQUIRED_PACKAGES = [
+    'streamlit==0.65.2',
+    'numpy==1.18.4',
+    'pandas==0.24.2',
+    'tensorflow>=2.3.0',
+    'matplotlib==3.2.1',
+    'plotly==4.6.0',
+    'scipy==1.2.2',
+    'requests==2.23.0',
+    'bs4==0.0.1',
+    'os',
+    'termcolor',
+    'keras-tuner',
+    'glob',
+    'sklearn',
+    'imageio',
+    'PIL',
+    'imblearn',
+    'warnings',
+    'json',
+    'joblib',
+    'google.cloud',
+    'google.oauth2'
+    'datetime'
+    ]
+
 
 setup(name='skin_lesion_detection',
       version="1.0",
+      install_requires=REQUIRED_PACKAGES,
       description="Project Description",
       packages=find_packages(),
       test_suite = 'tests',
-      # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
       scripts=['scripts/skin_lesion_detection-run'],
       zip_safe=False)
